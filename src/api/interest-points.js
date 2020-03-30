dcaeb59870dcb6325e25d731a255b5e2cc71a95c
Rui Fonseca <rui.fonseca@talkdesk.com>
2020-03-30 10:41:41 +0100

    Fix lint

--- src/api/interest-points.js
+++ src/api/interest-points.js
@@ -1,7 +1,14 @@
 export const getInterestPoints = () => {
-  return fetch("/interest-points").then(response => response.json()).then(json => json.interestPoints)
-}
+  return fetch("/interest-points")
+    .then((response) => response.json())
+    .then((json) => json.interestPoints);
+};
 
 export const createInterestPoint = (title, latitude, longitude) => {
-  return fetch("/interest-points", { method: "POST", body: JSON.stringify({ title, latitude, longitude }) }).then(response => response.json()).then(json => json.interestPoint)
-}
+  return fetch("/interest-points", {
+    method: "POST",
+    body: JSON.stringify({title, latitude, longitude}),
+  })
+    .then((response) => response.json())
+    .then((json) => json.interestPoint);
+};
--- src/index.js
+++ src/index.js
@@ -1,42 +1,54 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-import { Server, Model } from "miragejs"
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+import {Server, Model} from "miragejs";
 
 new Server({
   models: {
-    interestPoint: Model
+    interestPoint: Model,
   },
   routes() {
     this.get("/interest-points", (db) => {
-      return db.interestPoints.all()
-    })
+      return db.interestPoints.all();
+    });
 
     this.get("/interest-points/:id", (db, request) => {
-      const id = request.params.id
+      const id = request.params.id;
 
-      return db.interestPoints.find(id)
-    })
+      return db.interestPoints.find(id);
+    });
 
     this.post("/interest-points", (db, request) => {
-      const body = JSON.parse(request.requestBody)
-      const { title, latitude, longitude } = body;
+      const body = JSON.parse(request.requestBody);
+      const {title, latitude, longitude} = body;
 
-      const newInterestPoint = db.interestPoints.create({ title, latitude, longitude });
+      const newInterestPoint = db.interestPoints.create({title, latitude, longitude});
 
       return newInterestPoint;
-    })
+    });
   },
   seeds(server) {
-    server.schema.interestPoints.create({ title: "Casa da Música", latitude: 41.1589, longitude: -8.6307 })
-    server.schema.interestPoints.create({ title: "Torre de Belém", latitude: 38.6916, longitude: -9.2160 })
-    server.schema.interestPoints.create({ title: "Cabo da Roca", latitude: 38.7804, longitude: -9.4989 })
+    server.schema.interestPoints.create({
+      title: "Casa da Música",
+      latitude: 41.1589,
+      longitude: -8.6307,
+    });
+    server.schema.interestPoints.create({
+      title: "Torre de Belém",
+      latitude: 38.6916,
+      longitude: -9.216,
+    });
+    server.schema.interestPoints.create({
+      title: "Cabo da Roca",
+      latitude: 38.7804,
+      longitude: -9.4989,
+    });
   },
-})
+});
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
